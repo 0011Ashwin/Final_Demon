1080509ee7987b216062856118da1e72f0338384
Ashwin <ashwinmehta1234500@gmail.com>
2025-01-03 03:31:35 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ import pandas as pd
 import dill
 import pickle
 from sklearn.metrics import r2_score
-# from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -53,10 +53,10 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
     except Exception as e:
         raise CustomException(e, sys)
     
-# def load_object(file_path):
-#     try:
-#         with open(file_path, "rb") as file_obj:
-#             return pickle.load(file_obj)
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
-#     except Exception as e:
-#         raise CustomException(e, sys)
+    except Exception as e:
+        raise CustomException(e, sys)
